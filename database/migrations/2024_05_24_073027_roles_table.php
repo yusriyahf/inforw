a46742929a3ce40085a0df44b137afbcd229bb66
Yusriyah Firjatullah <57206360+yusriyahf@users.noreply.github.com>
2024-05-31 12:59:46 +0700

    penambahan role

--- database/migrations/2024_05_24_073027_roles_table.php
+++ database/migrations/2024_05_24_073027_roles_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id('role_id');
-            $table->enum('nama', ['admin', 'rw', 'rt', 'warga']);
+            $table->enum('nama', ['admin', 'rw', 'rt', 'sekretaris', 'bendahara', 'warga']);
             $table->timestamps();
         });
     }
