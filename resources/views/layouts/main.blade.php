d8e7a92ce9ab475192970a4a3c1eff530c32b586
astinurin <astihidayanti20@gmail.com>
2024-06-08 23:29:31 +0700

    edit footer

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -33,7 +33,7 @@
   <link href="{{ asset('/css/nucleo-svg.css') }}" rel="stylesheet" />
   <!-- CSS Files -->
   <link id="pagestyle" href="{{ asset('/css/argon-dashboard.css?v=2.0.4') }}" rel="stylesheet" />
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> --}}
 
   {{-- CHART --}}
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
